38fd880d581b128624cc84be3b3e8ecc1eb48963
fernandinho937 <fernandodrm@bol.com.br>
2023-04-26 21:01:18 -0300

    senhas criptografadas com sucesso

--- criptografar-senha/src/main/java/com/criptografar/criptografarsenha/controller/UsuarioController.java
+++ criptografar-senha/src/main/java/com/criptografar/criptografarsenha/controller/UsuarioController.java
@@ -1,10 +1,16 @@
 package com.criptografar.criptografarsenha.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.criptografar.criptografarsenha.model.UsuarioModel;
@@ -15,15 +21,39 @@ import com.criptografar.criptografarsenha.repository.UsuarioRepository;
 public class UsuarioController {
 
     private final UsuarioRepository repository;
-    
-    public UsuarioController(UsuarioRepository repository) {
+    private final PasswordEncoder encoder;
+
+    public UsuarioController(UsuarioRepository repository, PasswordEncoder encoder) {
         this.repository = repository;
+        this.encoder = encoder;
     }
+
     @GetMapping("/listarTodos")
    public ResponseEntity<List<UsuarioModel>> listarTodos() {
     return ResponseEntity.ok(repository.findAll());
-
    }
 
+    @PostMapping("/salvar")
+    public ResponseEntity<UsuarioModel> salvar(@RequestBody UsuarioModel usuario) {
+    usuario.setPassword(encoder.encode(usuario.getPassword()));
+        return ResponseEntity.ok(repository.save(usuario));
+   }
+   
+    @GetMapping("/validarsenha")
+    public ResponseEntity<Boolean> validarsenha(@RequestParam String login, @RequestParam String password) {
+                
+        Optional<UsuarioModel> optUsuario = repository.findByLogin(login);
+        if (optUsuario.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+        
+                
+        UsuarioModel usuario = optUsuario.get();
+        boolean valid = encoder.matches(password, usuario.getPassword());
+
+        HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+        return ResponseEntity.status(status).body(valid);
+    
     }
+}
 
